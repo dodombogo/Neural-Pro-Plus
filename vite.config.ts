dad399b109d0e4000f3410d5caed691337158016
oNeural <mbogodod@gmail.com>
2024-12-16 06:39:43 +0300

    fix: update base URL for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://oneural.github.io/Neural-Pro-Plus/',
+  base: '/Neural-Pro-Plus/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
