4a1850905f9ab8a59b2f0df20c49cedaf8007772
oNeural <mbogodod@gmail.com>
2024-12-16 07:19:23 +0300

    Updates

--- dist/assets/editor-vendor-CnHC8yA9.js
+++
@@ -1,2 +0,0 @@
-import"./react-vendor-BaU2RZxY.js";
-//# sourceMappingURL=editor-vendor-CnHC8yA9.js.map
--- src/env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_ASSEMBLY_API_KEY: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-} 
